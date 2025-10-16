7aa0bec4102922285ad0fb9a55eb6fc3419cc46e
Hazel-shaw <zihan.zhang@zhichun.org>
2025-10-16 12:50:01 +0800

    Update S03_Using_Theorems_and_Lemmas.lean

--- MIL/C02_Basics/S03_Using_Theorems_and_Lemmas.lean
+++ MIL/C02_Basics/S03_Using_Theorems_and_Lemmas.lean
@@ -47,7 +47,11 @@ example (x : ℝ) : x ≤ x :=
 
 -- Try this.
 example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e := by
-  sorry
+  apply lt_of_le_of_lt h₀
+  apply lt_trans h₁
+  apply lt_of_le_of_lt h₂
+  apply lt_of_lt_of_le h₃
+  apply le_refl
 
 example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e := by
   linarith
@@ -89,14 +93,21 @@ example (h₀ : a ≤ b) (h₁ : c < d) : a + exp c + e < b + exp d + e := by
     apply exp_lt_exp.mpr h₁
   apply le_refl
 
-example (h₀ : d ≤ e) : c + exp (a + d) ≤ c + exp (a + e) := by sorry
+example (h₀ : d ≤ e) : c + exp (a + d) ≤ c + exp (a + e) := by
+  apply add_le_add_left
+  rw[exp_le_exp]
+  --apply add_le_add_left a 不行？
+  apply add_le_add_left h₀
 
 example : (0 : ℝ) < 1 := by norm_num
 
 example (h : a ≤ b) : log (1 + exp a) ≤ log (1 + exp b) := by
-  have h₀ : 0 < 1 + exp a := by sorry
-  apply log_le_log h₀
-  sorry
+  have h₀ : 0 < 1 + exp a := by
+    linarith [exp_pos a] --0 < rexp x
+  apply log_le_log h₀ --apply add_le_add_left (exp_le_exp.mpr h)
+  apply add_le_add_left
+  rw[exp_le_exp]
+  apply h
 
 example : 0 ≤ a ^ 2 := by
   -- apply?
